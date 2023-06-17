23d1996f707595209d651895cbf82e88daee7272
Blockchain-Key <1416893908@qq.com>
2023-06-17 17:49:22 +0800

    修改footer图标加载

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,45 @@ import ins from '@/assets/in.png'
 import fb from '@/assets/fb.png'
 import github from '@/assets/github.png'
 
+const links = [
+  {
+    name: 'twitter',
+    imgSrc: twitter,
+    link: '#',
+  },
+  {
+    name: 'in',
+    imgSrc: ins,
+    link: '#',
+  },
+  {
+    name: 'facebook',
+    imgSrc: fb,
+    link: '#',
+  },
+  {
+    name: 'github',
+    imgSrc: github,
+    link: '#',
+  },
+]
+
 export default function Footer() {
   return (
     <footer className="h-[85px] flex justify-between items-center border-solid border-[#7F56D9] border-0 border-t mt-auto">
       <div className="text-[#475467]">© 2023 Brickin. All rights reserved.</div>
       <div className="flex">
-        <img src={twitter} alt="twitter" className="h-[24px] mx-2 cursor-pointer" />
-        <img src={ins} alt="in" className="h-[24px] mx-2 cursor-pointer" />
-        <img src={fb} alt="facebook" className="h-[24px] mx-2 cursor-pointer" />
-        <img src={github} alt="github" className="h-[24px] mx-2 cursor-pointer" />
+        {links.map((item, index) => {
+          return (
+            <img
+              key={index}
+              src={item.imgSrc}
+              alt={item.name}
+              className="h-[24px] mx-2 cursor-pointer"
+              onClick={() => console.log(item.link)}
+            />
+          )
+        })}
       </div>
     </footer>
   )
