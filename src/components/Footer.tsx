e1508e0ae17c59ef1438fe5d222e4fcadcd7a7dc
Blockchain-Key <1416893908@qq.com>
2023-06-17 17:42:49 +0800

    修改footer自适应

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import github from '@/assets/github.png'
 
 export default function Footer() {
   return (
-    <footer className="h-[85px] flex justify-between items-center border-solid border-[#7F56D9] border-0 border-t">
+    <footer className="h-[85px] flex justify-between items-center border-solid border-[#7F56D9] border-0 border-t mt-auto">
       <div className="text-[#475467]">© 2023 Brickin. All rights reserved.</div>
       <div className="flex">
         <img src={twitter} alt="twitter" className="h-[24px] mx-2 cursor-pointer" />
