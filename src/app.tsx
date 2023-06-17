e0c0d49c60c8086247b6d7cd8bc40ad4c7fd381b
Blockchain-Key <1416893908@qq.com>
2023-06-17 16:44:21 +0800

    修改钱包配置

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,5 @@
-import { WalletProvider, SuiWallet, SuietWallet, EthosWallet, MartianWallet } from '@suiet/wallet-kit'
+import { WalletProvider } from '@suiet/wallet-kit'
 import '@suiet/wallet-kit/style.css'
 export function rootContainer(container: any) {
-  return (
-    <WalletProvider defaultWallets={[SuiWallet, SuietWallet, EthosWallet, MartianWallet]}>{container}</WalletProvider>
-  )
+  return <WalletProvider>{container}</WalletProvider>
 }
