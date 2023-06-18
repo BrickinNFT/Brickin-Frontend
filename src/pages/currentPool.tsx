debe185a824fa09c56fc4dbcf103c1363d58562e
Blockchain-Key <1416893908@qq.com>
2023-06-18 12:28:08 +0800

    抽离nav组件

--- src/pages/currentPool.tsx
+++ src/pages/currentPool.tsx
@@ -1,18 +1,11 @@
 import React, { useState } from 'react'
-import checkBox from '@/assets/checkBox.png'
+import Nav from '@/components/Nav'
+
 import pool_1 from '@/assets/pool_1.png'
 import pool_2 from '@/assets/pool_2.png'
+
 export default function currentPool() {
   const [currentComponent, setCurrentComponent] = useState('Pool')
-  const [nav, setNav] = useState([
-    { name: 'Colletions', link: '' },
-    { name: 'Pool', link: '' },
-    { name: 'Changelog', link: '' },
-    { name: 'Slack Community', link: '' },
-    { name: 'Suppport', link: '' },
-    { name: 'API', link: '' },
-    { name: 'Log out', link: '' },
-  ])
   const [poolInfo, setPoolInfo] = useState({
     trading: 1,
     buy: 0,
@@ -43,45 +36,7 @@ export default function currentPool() {
   return (
     <div className="flex justify-center items-center">
       <div className="flex-[3]">
-        <nav className="flex items-center">
-          <div className=" rounded-2xl flex flex-col items-start w-[272px] bg-white border border-[#EAECF0] border-solid">
-            <ul className="w-full">
-              {nav.map((item, index) => {
-                if (item.name === 'Changelog') {
-                  return (
-                    <li
-                      key={index}
-                      className="p-3 border-0 border-[#EAECF0] border-solid w-full border-t cursor-pointer">
-                      {item.name}
-                    </li>
-                  )
-                }
-                if (item.name === 'API') {
-                  return (
-                    <li
-                      key={index}
-                      className="p-3 border-0 border-[#EAECF0] border-solid w-full border-b cursor-pointer">
-                      {item.name}
-                    </li>
-                  )
-                }
-                if (item.name === currentComponent) {
-                  return (
-                    <li key={index} className="flex items-center p-3 cursor-pointer">
-                      <img src={checkBox} alt="" className="w-6 h-6 mr-2" />
-                      {item.name}
-                    </li>
-                  )
-                }
-                return (
-                  <li key={index} className="p-3 cursor-pointer">
-                    {item.name}
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-        </nav>
+        <Nav currentComponent={currentComponent}></Nav>
       </div>
       <div className="flex-[9]">
         <div className="text-gray font-bold flex flex-col items-center">
